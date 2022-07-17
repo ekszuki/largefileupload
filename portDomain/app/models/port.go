15b00cf3313bd03d21a10ad5b2ac1f063489c1d0
Jefferson Zuchi <jefferson@sygnux.com.br>
2022-07-17 20:26:29 +0000

    port domain model

---
+++ portDomain/app/models/port.go
@@ -0,0 +1,16 @@
+package models
+
+// Domain Port ...
+type Port struct {
+	Key         string
+	Name        string
+	City        string
+	Country     string
+	Alias       []string
+	Regions     []string
+	Coordinates []float64
+	Province    string
+	Timezone    string
+	Unlocs      []string
+	Code        string
+}
