d0fb2d2f642888b6b6aabf81fe95bb81284d70bf
Jefferson Zuchi <jefferson@sygnux.com.br>
2022-07-18 01:48:32 +0000

    parse port domain to FindByKeyResponse

--- portDomain/app/parsers/transformers.go
+++ portDomain/app/parsers/transformers.go
@@ -12,13 +12,31 @@ func FromUpLoadPortRequestToDomain(req *protoport.UpLoadPortRequest) *models.Por
 	dmPort.Name = req.GetName()
 	dmPort.City = req.GetCity()
 	dmPort.Country = req.GetCountry()
-	dmPort.Alias = append(dmPort.Alias, req.Alias...)
-	dmPort.Regions = append(dmPort.Regions, req.Regions...)
-	dmPort.Coordinates = append(dmPort.Coordinates, req.Coordinates...)
+	dmPort.Alias = req.Alias
+	dmPort.Regions = req.Regions
+	dmPort.Coordinates = req.Coordinates
 	dmPort.Province = req.GetProvince()
 	dmPort.Timezone = req.GetTimezone()
-	dmPort.Unlocs = append(dmPort.Unlocs, req.Unlocs...)
+	dmPort.Unlocs = req.Unlocs
 	dmPort.Code = req.Code
 
 	return dmPort
+}
+
+func FromDomainToFindByKeyResponse(dmPort *models.Port) *protoport.FindByKeyResponse {
+	resp := new(protoport.FindByKeyResponse)
+
+	resp.Key = dmPort.Key
+	resp.Name = dmPort.Name
+	resp.City = dmPort.City
+	resp.Country = dmPort.Country
+	resp.Alias = dmPort.Alias
+	resp.Regions = dmPort.Regions
+	resp.Coordinates = dmPort.Coordinates
+	resp.Province = dmPort.Province
+	resp.Timezone = dmPort.Timezone
+	resp.Unlocs = dmPort.Unlocs
+	resp.Code = dmPort.Code
+
+	return resp
 }
