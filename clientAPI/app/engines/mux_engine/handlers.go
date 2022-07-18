ba36995f267bcbe0540a6e2dff0b16c2bb8210d1
Jefferson Zuchi <jefferson@sygnux.com.br>
2022-07-18 03:03:03 +0000

    find port by key handler implementation

--- clientAPI/app/engines/mux_engine/handlers.go
+++ clientAPI/app/engines/mux_engine/handlers.go
@@ -2,12 +2,15 @@ package muxengine
 
 import (
 	"ekszuki/uploader/clientAPI/app/core"
+	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
-// UpLoadPortsHandler implements contracts.PortApi
+// UpLoadFilePortsHandler Upload large json file
 func (s *MuxEnginer) UpLoadFilePortsHandler(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 	logCtx := logrus.WithFields(
@@ -24,4 +27,43 @@ func (s *MuxEnginer) UpLoadFilePortsHandler(w http.ResponseWriter, req *http.Req
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+// FindPortByKeyHandler - Find Port by key
+func (s *MuxEnginer) FindPortByKeyHandler(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	logCtx := logrus.WithFields(
+		logrus.Fields{"package": "muxenginer", "function": "FindPortByKeyHandler"},
+	)
+	logCtx.Info("starting request")
+
+	vars := mux.Vars(req)
+	key := vars["key"]
+
+	if key == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("key is invalid"))
+		return
+	}
+
+	portCore := core.NewPort(s.client)
+	resp, err := portCore.FindByKey(ctx, key)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("something went wrong"))
+		return
+	}
+
+	bs, err := json.Marshal(resp)
+	if err != nil {
+		logCtx.Errorf("error on parse json response, %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("something went wrong"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("x-server-time", time.Now().UTC().Format(time.RFC3339))
+	w.WriteHeader(http.StatusOK)
+	w.Write(bs)
 }
--- clientAPI/app/engines/mux_engine/mux.go
+++ clientAPI/app/engines/mux_engine/mux.go
@@ -20,6 +20,7 @@ func NewMuxEnginer(portGRPCClient protoport.PortServiceClient) *MuxEnginer {
 func (m *MuxEnginer) CreateMuxEnginer() http.Handler {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/port/fileupload", m.UpLoadFilePortsHandler).Methods(http.MethodPost)
+	r.HandleFunc("/port/{key}", m.FindPortByKeyHandler).Methods(http.MethodGet)
 
 	return r
 }
